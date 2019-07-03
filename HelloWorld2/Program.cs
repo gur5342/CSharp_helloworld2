887d69f11240f340ef5a40381e2a47cf9b5f5b11
hsh <sung5342@hanmail.net>
2019-07-03 12:30:29 +0900

    Add project files.

---
+++ HelloWorld2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            Console.WriteLine("hello world2");
+
+            int a = 1;
+            a += 2;
+            a++;
+
+            Console.WriteLine(a);
+        }
+    }
+}
